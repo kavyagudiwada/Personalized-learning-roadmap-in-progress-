8b1be8ecabfb0fea6d009a87111776683d313858
kavyagudiwada <kavyagudiwada18@gmail.com>
2026-07-03 17:40:32 +0530

    FIX CORS ORIGIN

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,6 +22,7 @@ const allowedOrigins = [
 	process.env.FRONTEND_URL || "http://localhost:5173",
 	"http://localhost:5173",
 	"http://localhost:3000",
+
 ];
 
 
@@ -29,7 +30,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "personalized-learning-roadmap-in-progress-u8od-16uc5hqjt.vercel.app",
+      "https://personalized-learning-roadmap-in-progress-u8od-16uc5hqjt.vercel.app",
     ],
     credentials: true,
   })
